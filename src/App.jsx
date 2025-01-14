9a423e8c96c75f93d461aa65e05c712e571d0b44
unknown <anamikaregmi1@.com>
2025-01-15 04:00:34 +0545

    there's an error in the css file

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
-import Message from " ./components/Message";
+import Message from "./message";
 
 function App() {
-  return <div><Message /></div>;
+  return (
+    <div className="flex justify-center items-center h-screen bg-gray-100">
+      <h1 className="text-4xl font-bold text-blue-500">
+      <Message>Hello, Tailwind CSS!</Message>
+      </h1>
+    </div>
+  );
 }
 
 export default App;
